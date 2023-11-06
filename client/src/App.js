6075937e304db3aa1316f400ceee934c5f6fc6c4
rushaliagarwal <rushaliagarwal99@gmail.com>
2023-11-06 20:03:15 +0530

    quiz main page created

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './styles/App.css';
+import { createBrowserRouter , RouterProvider} from 'react-router-dom'
+//import components
+
+import Main from './pages/quiz/Main';
+import Quiz from './pages/quiz/Quiz';
+import Result from './pages/quiz/Result';
+// react routes//
+ const router  = createBrowserRouter([
+  {
+    path :'/',
+    element: <Main></Main>
+  },
+  {
+    path :'/quiz',
+    element: <Quiz></Quiz>
+  },
+  {
+    path :'/result',
+    element: <Result></Result>
+  },
+ ]
+ )
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
---
+++ client/src/pages/quiz/Quiz.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Quiz() {
+  return (
+    <div className='container'>
+        <h1 className='title text-light'>Quiz</h1>
+
+    </div>
+  )
+}
