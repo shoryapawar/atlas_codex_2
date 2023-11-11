c6a15fa239a1327175636e26806f970ddf92da22
Shorya Pawar <22shorya23@gmail.com>
2023-11-11 17:56:00 +0530

    added Gsap in npm and News page
    gsap not working have to fix

--- client/src/pages/News/News.js
+++ client/src/pages/News/News.js
@@ -5,10 +5,12 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { useEffect, useState } from "react";
-// import Navbar from "../../components/Navbar/Navbar";
+import { useEffect, useState, useLayoutEffect, useRef } from "react";
 import axios from "axios";
 import "./News.css";
+// gsap
+import { gsap } from "gsap";
+
 const Newspage = () => {
   const [loading, setLoading] = useState(true);
   const [articleRes, setartcileRes] = useState(null);
@@ -25,44 +27,67 @@ const Newspage = () => {
         setLoading(false);
       });
   }, []);
+
+  // gsap code
+  const el = useRef();
+  const tl = useRef();
+
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      tl.current = gsap
+        .timeline()
+        .from(".article", {
+          x: -800,
+          opacity: 0,
+          duration: 1,
+          delay: 1,
+        })
+        .from(".card", {
+          yoyoEase: true,
+          opacity: 0.4,
+          duration: 0.3,
+          delay: 0.8,
+        });
+    }, el);
+  }, [loading]);
+
   return (
     <>
-      {/* <Navbar></Navbar> */}
       <div className="page">
         {loading ? (
           <p>Loading Please Wait</p>
         ) : (
           <div>
-            <h1 style={{color: 'white'}}> Space News and Articles</h1>
+            <h1 style={{ color: "white" }}> Space News and Articles</h1>
             <ul className="article">
               {articleRes.map((article) => (
-                <div key={article.id}>
-                <Card sx={{ maxWidth: 345 }}>
-                  <CardMedia
-                    sx={{ height: 140 }}
-                    image={article.image_url}
-                    title={article.title}
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {article.title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      {article.summary}
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <a
-                      href={article.url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <Button src={article.url} size="small">
-                        Know More
-                      </Button>
-                    </a>
-                  </CardActions>
-                </Card>
+                <div key={article.id} className="card">
+                  <Card sx={{ maxWidth: 345 }}>
+                    <CardMedia
+                      sx={{ height: 140 }}
+                      image={article.image_url}
+                      title={article.title}
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        {article.title}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        {article.summary}
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <a
+                        href={article.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <Button src={article.url} size="small">
+                          Know More
+                        </Button>
+                      </a>
+                    </CardActions>
+                  </Card>
                 </div>
               ))}
             </ul>
