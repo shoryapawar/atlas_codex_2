feb1e5b7221a6d4492c1843e4154f8b1ef331f4c
Shorya Pawar <22shorya23@gmail.com>
2023-11-15 19:21:40 +0530

    added lesson cards

--- client/src/pages/Lessons/index.js
+++ client/src/pages/Lessons/index.js
@@ -1,6 +1,17 @@
-import "./lesson.scss"
+import "./lesson.scss";
+import { lesson } from "../../sample.js";
+import LessonCard from "./LessonCard";
 const index = () => {
-  return <div className="lesson-main">Lesson</div>;
+  return (
+    <div className="lesson-main">
+      <h1>Lessons</h1>
+      <div className="lessons-container">
+        {lesson.map((lesson, index) => (
+          <LessonCard key={index} {...lesson} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default index;
